22acb87901ddc021b575e8d23ff449a229fca79b
Shubham Duhan <shubham.duhan@creditas.in>
2026-03-20 01:46:01 +0530

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/knockit_landing/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
